e3701abd4fe368a895d5bcc02fee75bb01e24683
Mikael Gourine <mikael.gourine@edu.karelia.fi>
2022-10-12 10:11:32 +0300

    Initial commit

---
+++ rest1.js
@@ -0,0 +1,83 @@
+const express = require("express");
+
+const fs = require("fs");
+
+const app = express();
+
+const port = 3000;
+
+let sanakirja = [];
+
+let data = fs.readFileSync("./sanakirja.txt", { encoding: "utf8", flag: "r" });
+
+const splitLines = data.split(/\r?\n/);
+
+splitLines.forEach((line) => {
+  const sanat = line.split(" ");
+  //console.log(sanat);
+
+  const sana = {
+    fin: sanat[0],
+
+    eng: sanat[1],
+  };
+
+  //console.log(sana);
+
+  sanakirja.push(sana);
+});
+
+console.log(sanakirja);
+
+//console.log(splitLines);
+
+app.use(express.json()); //käytetään json muotoa
+
+app.use(express.urlencoded({ extended: true })); //käytetään tiedonsiirrossa laajennettua muotoa
+
+//CORS asetukset
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+
+    "Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+
+  // to the API (e.g. in case you use sessions)
+
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  res.setHeader("Content-type", "application/json");
+
+  next();
+});
+
+app.get("/sanakirja", (req, res) => {
+  res.json(sanakirja); //palautetaan sanakirja taulukko json muodossa
+});
+
+app.post("/sanakirja", (req, res) => {
+
+  console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
